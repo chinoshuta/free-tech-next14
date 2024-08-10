a2b687e085b5e9575c4ec7aae3e00ada82d0c150
chinoshuta <xchino-shu@booklista.co.jp>
2024-08-10 12:49:16 +0900

    ヘッダーをスクロールで非表示にする

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,29 @@
+"use client";
 
 import React from "react";
 import styles from "./index.module.scss";
 import Link from "next/link";
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
+import clsx from "clsx";
 
 const Header: React.FC = () => {
+  const [isHeaderHide, seIsHeaderHide] = React.useState<boolean>(false);
+
+  useScrollPosition(({ prevPos, currPos }) => {
+    if (currPos.y >= 0) {
+      seIsHeaderHide(false);
+      return;
+    }
+    if (prevPos.y > currPos.y) {
+      // 下スクロールの場合はヘッダー非表示
+      seIsHeaderHide(true);
+      return;
+    }
+    seIsHeaderHide(false);
+  }, []);
+
   return (
-    <header className={styles.root}>
+    <header className={clsx(styles.root, isHeaderHide && styles.hidden)}>
       <div className={styles.wrapper}>
         <Link href="/">
           <h1 className={styles.title}>free 技術log</h1>
