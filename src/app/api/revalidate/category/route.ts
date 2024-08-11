743bb335d79bac13d6ca63aee28753b5a0d27a3d
chino <xchino-shu@booklista.co.jp>
2024-08-11 10:13:51 +0900

    update route.ts

--- src/app/api/revalidate/category/route.ts
+++ src/app/api/revalidate/category/route.ts
@@ -1,9 +1,7 @@
 import { revalidateTag } from "next/cache";
 import { NextResponse, type NextRequest } from "next/server";
 
-export async function POST(req: NextRequest) {
-  const params = await req.json();
-  revalidateTag("posts");
-  revalidateTag(`post-${params.contents?.new?.id}`);
+export async function POST() {
+  revalidateTag("categories");
   return new NextResponse("revalidate tag", { status: 200 });
 }
